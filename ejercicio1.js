45e04efe2ebcb82c68eb3063f97ffad9f8164775
Agustina404 <agustinacorreo@hotmail.com>
2024-07-17 16:25:46 +0200

    OK

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,9 +1,12 @@
+//EJERCICIO 1
 let miSuperheroeFavorito = 'Hulk'
 console.log(miSuperheroeFavorito)
 
+//EJERCICIO 2
 let numeroFavorito = 50
 console.log(numeroFavorito)
 
+//EJERCICIO 3
 let altura = 5
 console.log(altura)
 
--- ejercicio2.js
+++ ejercicio2.js
@@ -1,8 +1,11 @@
+//EJERCICIO 1
 const jedi = { nombre: 'Luke Skywalker', edad: 19 }
 
 jedi.edad = 25
 
 console.log(jedi)
+
+//EJERCICIO2
 
 let nombre = 'Leia'
 let apellido = ' Organa'
@@ -16,11 +19,15 @@ console.log(
     ' y soy una princesa de Alderaan.'
 )
 
+//EJERCICIO 3
+
 const sable1 = { nombre: 'Shoto de Yoda', precio: 1500 }
 const sable2 = { nombre: 'Sable de Darth Vader', precio: 2000 }
 
 let total = sable1.precio + sable2.precio
 console.log('El precio total de los dos sables es: ' + total)
+
+//EJERCICIO 4
 
 let precioBaseGlobal = 25000
 
