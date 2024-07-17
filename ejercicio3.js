e37541d099ab2192482800e8e4114ccc7eced629
Agustina404 <agustinacorreo@hotmail.com>
2024-07-17 16:39:43 +0200

    ok

---
+++ ejercicio3.js
@@ -0,0 +1,25 @@
+//EJERCICIO 1
+let multiplicacion = 10 * 5
+console.log(multiplicacion)
+
+//EJERCICIO2
+let division = 10 / 2
+console.log(division)
+
+//EJERCICIO3
+let restoDedivision = 15 % 9
+console.log(restoDedivision)
+
+//EJERCICIO4
+let p = 10
+let j = 5
+let o = p + j
+
+console.log(o)
+
+//EJERCICIO5
+let c = 10
+let m = 5
+let i = 10 * 5
+
+console.log(i)
