70dc262576b2e7a5c5137ee7e95aaab9591ee782
Agustina404 <agustinacorreo@hotmail.com>
2024-07-17 16:17:59 +0200

    ok

--- ejercicio2.js
+++ ejercicio2.js
@@ -21,3 +21,21 @@ const sable2 = { nombre: 'Sable de Darth Vader', precio: 2000 }
 
 let total = sable1.precio + sable2.precio
 console.log('El precio total de los dos sables es: ' + total)
+
+let precioBaseGlobal = 25000
+
+const nave1 = { nombre: 'Ala-X', precioBase: 50000, precioFinal: 60000 }
+const nave2 = {
+  nombre: 'Halcón Milenario',
+  precioBase: 70000,
+  precioFinal: 80000
+}
+
+let totalNave1 = precioBaseGlobal + nave1.precioBase
+let totalNave2 = precioBaseGlobal + nave2.precioBase
+console.log(
+  'El precio final de Ala-X es = ' +
+    totalNave1 +
+    ' y el precio final de Halcón Milenario es = ' +
+    totalNave2
+)
