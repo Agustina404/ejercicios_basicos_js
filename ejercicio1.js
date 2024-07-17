ccd8078595f74bbe4dbd8f2f1c4c7f1a819fc873
Agustina404 <agustinacorreo@hotmail.com>
2024-07-17 15:38:12 +0200

    ok

---
+++ ejercicio1.js
@@ -0,0 +1,14 @@
+let miSuperheroeFavorito = 'Hulk'
+console.log(miSuperheroeFavorito)
+
+let numeroFavorito = 50
+console.log(numeroFavorito)
+
+let altura = 5
+console.log(altura)
+
+let longitud = 10
+console.log(longitud)
+
+let suma = altura + longitud
+console.log(suma)
