f697579cec6879668580aba9f6c3581428fa7ce7
Agustina404 <agustinacorreo@hotmail.com>
2024-07-17 15:49:28 +0200

    ok

--- ejercicio2.js
+++ ejercicio2.js
@@ -3,3 +3,15 @@ const jedi = { nombre: 'Luke Skywalker', edad: 19 }
 jedi.edad = 25
 
 console.log(jedi)
+
+let nombre = 'Leia'
+let apellido = ' Organa'
+let edad = 20
+console.log(
+  'Soy ' +
+    nombre +
+    apellido +
+    ', tengo ' +
+    edad +
+    ' y soy una princesa de Alderaan.'
+)
