7adb664c34bae217be2d378d6d6eeb3804f217f0
Agustina404 <agustinacorreo@hotmail.com>
2024-07-18 16:53:54 +0200

    ok

---
+++ ejercicio6.js
@@ -0,0 +1,13 @@
+//EJERCICIO1
+//for (let i = 0; i <= 9; i++) {
+//console.log(i)
+//}
+
+//EJERCICIO2
+//for (let i = 0; i <= 9; i++) {
+//if (i % 2 === 0) {
+//console.log(i)
+//}
+//}
+
+//EJERCICIO3
