6e832d193f3b9d34b1cc6faa66d684f0398f360c
Agustina404 <agustinacorreo@hotmail.com>
2024-07-18 12:28:23 +0200

    ok

---
+++ ejercicio4.js
@@ -0,0 +1,16 @@
+//EJERCICIO1
+const aldeanos = ['Fibrilio', 'Narciso', 'Vacarena', 'Tendo', 'Nendo']
+let remove1 = 3
+let remove2 = 1
+aldeanos.splice(remove1, remove2)
+console.log(aldeanos)
+
+//EJERCICIO2
+
+//EJERCICIO3
+
+//EJERCICIO4
+
+//EJERCICIO5
+
+//EJERCICIO6
