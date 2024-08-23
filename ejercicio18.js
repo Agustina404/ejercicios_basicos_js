f9e6a5bf6f34d7dac20794c3cafa3e49dbbfdb6f
Agustina404 <agustinacorreo@hotmail.com>
2024-08-23 10:57:37 +0200

    ok

---
+++ ejercicio18.js
@@ -0,0 +1,13 @@
+const placesToTravel = [
+  { id: 5, name: 'Japan' },
+  { id: 11, name: 'Venecia' },
+  { id: 23, name: 'Murcia' },
+  { id: 40, name: 'Santander' },
+  { id: 44, name: 'Filipinas' },
+  { id: 59, name: 'Madagascar' }
+]
+
+placesToTravel.splice(1, 1)
+placesToTravel.splice(2, 1)
+
+console.log(placesToTravel)
